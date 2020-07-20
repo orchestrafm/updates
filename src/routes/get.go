efb9bfeb2b69931a010af9a3b83bd573d5b57ff1
Starz0r <starz0r@starz0r.com>
2020-07-20 18:17:38 -0500

    routes: list all updates past the given version

---
+++ src/routes/get.go
@@ -0,0 +1,35 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/orchestrafm/updates/src/database"
+	"github.com/spidernest-go/logger"
+	"github.com/spidernest-go/mux"
+)
+
+func listUpdates(c echo.Context) error {
+	ver, err := strconv.ParseUint(c.FormValue("version"), 10, 32)
+	if err != nil {
+		logger.Error().
+			Err(err).
+			Msg("Version number was not a valid uint64")
+
+		return c.JSON(http.StatusNotAcceptable, &struct {
+			Message string
+		}{
+			Message: "Version was not a valid number."})
+	}
+
+	p, err := database.SelectIDGreaterThan(ver)
+	if err != nil {
+		logger.Error().
+			Err(err).
+			Msg("Found no patches or database error")
+
+		return c.JSON(http.StatusOK, nil)
+	}
+
+	return c.JSON(http.StatusOK, p)
+}
