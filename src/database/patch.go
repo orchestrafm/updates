2a281e4df0d3c511fb52f848e5f9f5cdace6d87d
Starz0r <starz0r@starz0r.com>
2020-08-08 19:33:36 -0500

    database: update patch type struct to include additional details

--- src/database/patch.go
+++ src/database/patch.go
@@ -1,9 +1,13 @@
 package database
 
 type Patch struct {
-	ID          uint64 `db:"id" json:"id"`
-	Application string `db:"app" json:"app"`
-	Name        string `db:"name" json:"name"`
-	Platform    string `db:"platform" json:"platform"`
-	URL         string `db:"url" json:"url"`
+	ID            uint64 `db:"id" json:"id"`
+	Application   string `db:"app" json:"app"`
+	Name          string `db:"name" json:"name"`
+	Platform      string `db:"platform" json:"platform"`
+	Issuer        uint64 `db:"issuer" json:"issuer"`
+	URL           string `db:"url" json:"url"`
+	Hash          string `db:"url_hash" json:"hash"`
+	Signature     string `db:"sig_url" json:"sig"`
+	SignatureHash string `db:"sig_url_hash" json:"sig_hash"`
 }
