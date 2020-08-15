140f8aad1710105de629849cab93a0d8b7504ae1
Starz0r <starz0r@starz0r.com>
2020-08-14 19:47:22 -0500

    routes: allow checking for a user's role

--- src/routes/auth.go
+++ src/routes/auth.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"errors"
 	"net/http"
 	"strings"
 
@@ -14,6 +15,11 @@ type jwtExtendedClaims struct {
 	RealmAccess struct {
 		Roles []string `json:"roles,omitempty"`
 	} `json:"realm_access,omitempty"`
+	ResourceAccess struct {
+		ApplicationServices struct {
+			Roles []string `json:"roles,omitempty"`
+		} `json:"application-services,omitempty"`
+	} `json:"resource_access,omitempty"`
 	Scope             string `json:"scope"`
 	PreferredUsername string `json:"preferred_username,omitempty"`
 	Audience          string `json:"aud,omitempty"`
@@ -24,6 +30,34 @@ type jwtExtendedClaims struct {
 	NotBefore         int64  `json:"nbf,omitempty"`
 	Subject           string `json:"sub,omitempty"`
 	//jwt.StandardClaims
+}
+
+var (
+	ErrAuthParse   = errors.New("authorization token could not be parsed")
+	ErrPermissions = errors.New("insufficient permissions")
+)
+
+func HasRole(c echo.Context, reqrole string) bool {
+	user := c.Get("user").(*jwt.Token)
+	claims := new(jwtExtendedClaims)
+	err := decodeToClaims(user.Claims, claims)
+	if err != nil {
+		logger.Warn().
+			Err(err).
+			Msg("Claims were malformed or nonexistant, returning false.")
+		return false
+	}
+
+	for _, role := range claims.
+		ResourceAccess.
+		ApplicationServices.
+		Roles {
+		if role == reqrole {
+			return true
+		}
+	}
+
+	return false
 }
 
 func decodeToClaims(src, dst interface{}) error {
@@ -61,8 +95,8 @@ func FullAuthCheck(c echo.Context) error {
 			Message: "Authorization Token could not be parsed."})
 	}
 
-	auth := strings.Contains(claims.Scope, "track:write") || strings.Contains(claims.Scope, "track:admin")
-	admin := strings.Contains(claims.Scope, "track:admin")
+	auth := strings.Contains(claims.Scope, "update:write") || strings.Contains(claims.Scope, "update:admin")
+	admin := strings.Contains(claims.Scope, "update:admin")
 	if !admin || !auth {
 		return c.JSON(http.StatusUnauthorized, &struct {
 			Message string
@@ -81,8 +115,8 @@ func AuthorizationCheck(c echo.Context) (bool, bool) {
 		return false, false
 	}
 
-	auth := strings.Contains(claims.Scope, "track:write") || strings.Contains(claims.Scope, "track:admin")
-	admin := strings.Contains(claims.Scope, "track:admin")
+	auth := strings.Contains(claims.Scope, "update:write") || strings.Contains(claims.Scope, "update:admin")
+	admin := strings.Contains(claims.Scope, "update:admin")
 
 	return admin, auth
 }
